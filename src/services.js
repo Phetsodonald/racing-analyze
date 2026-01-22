c5d6db9e82589db32aa692fb699f31c5122004fa
Phetsodonald <donaldphetso69@gmail.com>
2026-01-22 17:11:28 +0200

    removed console.logs

--- src/services.js
+++ src/services.js
@@ -5,18 +5,57 @@ const BASE_URL = 'https://api.openf1.org/v1';
 async function getDriversData() {
   try {
     const res = await axios.get(`${BASE_URL}/drivers`);
-    console.log(res.data);
+    return res.data;
   } catch (error) {
-    console.error(error.message);
+    throw new Error(error.message);
   }
 }
 
 async function getSessionData() {
     try {
         const res = await axios.get(`${BASE_URL}/sessions`);
-        console.log(res.data)
+        return res.data
     } catch (error) {
-        console.error(error.message)
+        throw new Error(error.message)
     }
 }
 
+async function getLapsData() {
+  try {
+    const res = await axios.get(`${BASE_URL}/laps`, {
+      params: {
+        session_key: 9158,
+        driver_number: 44 
+      }
+    });
+
+    return res.data
+  } catch (error) {
+    throw new Error(error.message)
+  }
+}
+
+
+async function getCarData() {
+  try {
+    const res = await axios.get(`${BASE_URL}/car_data`, {
+      params: {
+        session_key: 9158,
+        driver_number: 1
+      }
+    });
+
+    return res.data.slice(0, 5)
+  } catch (error) {
+    throw new Error(error.message)
+  }
+}
+
+
+getCarData()
+  .then(data => {
+    console.log('Sample telemetry:', data);
+  })
+  .catch(err => {
+    console.error(err.message);
+  });
