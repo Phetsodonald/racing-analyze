714250aa6aa487b00b742289d49133958edb79b2
Phetsodonald <donaldphetso69@gmail.com>
2026-02-03 13:48:40 +0200

    updated the server.js

--- server.js
+++ server.js
@@ -2,19 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 
-const apiRoutes = require('./routes/routes');
+const apiRoutes = require('./src/routes/routes');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-
-
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', apiRoutes);
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
